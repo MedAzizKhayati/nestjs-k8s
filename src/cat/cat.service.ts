27ffee1ab12a155c3cd9ea1cb55a13642dfdf089
Mohamed Aziz Khayati <mohamed.aziz.khayati@divizend.com>
2023-11-20 21:58:11 +0100

    fix: move to t2.small from t2.micro

--- src/cat/cat.service.ts
+++ src/cat/cat.service.ts
@@ -45,7 +45,7 @@ export class CatService {
   async findRandoms(length: string): Promise<CatDocument[]> {
     const randoms = [];
 
-    for (let i = 0; i < Math.min(parseInt(length), 10); i++) {
+    for (let i = 0; i < Math.min(parseInt(length), 3); i++) {
       randoms.push(await this.findRandom());
     }
 
