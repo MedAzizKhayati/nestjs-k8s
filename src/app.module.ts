3711f73928503829c59aefb393e77a472980885b
Mohamed Aziz Khayati <mohamed.aziz.khayati@divizend.com>
2023-11-20 16:38:20 +0100

    feat: added mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
